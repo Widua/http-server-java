e7b7ff8ca084b4b36b813eab50d39e6dc2542f7e
Widua <mikolaj.widla@gmail.com>
2025-07-12 16:21:07 +0200

    added handlers, and 200 OK status response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,30 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        System.out.println("Logs from your program will appear here!");
+        final String HTTP_VERSION = "HTTP/1.1 ";
+        final String STATUS = "200 OK";
+        try {
+            ServerSocket serverSocket = new ServerSocket(4221);
+            serverSocket.setReuseAddress(true);
 
-     try {
-       ServerSocket serverSocket = new ServerSocket(4221);
+            Socket clientSocket = serverSocket.accept();
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+            try(
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    ){
+                 out.println(HTTP_VERSION+STATUS+"\r\n\r\n");
+            }
+            System.out.println("accepted new connection");
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
