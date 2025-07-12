69fe75410ca8b49ea8630fb7b64a60ed608a5bf4
Widua <mikolaj.widla@gmail.com>
2025-07-12 18:48:59 +0200

    parsing path added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/connection/ClientHandler.java
+++ src/main/java/connection/ClientHandler.java
@@ -1,22 +1,48 @@
 package connection;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ClientHandler implements Runnable {
-    private final String HTTP_VERSION = "HTTP/1.1 ";
+    private final String HTTP_VERSION = "HTTP/1.1";
     private PrintWriter output;
     private BufferedReader input;
 
-
-    public ClientHandler(PrintWriter output, BufferedReader input){
+    public ClientHandler(PrintWriter output, BufferedReader input) {
         this.output = output;
         this.input = input;
     }
 
     @Override
-    public void run() {
+    public void run(){
+        String httpStatus = "200 OK";
         System.out.println("accepted new connection");
-        output.println(HTTP_VERSION+"200 OK\r\n\r\n");
+        try {
+            Map<String,String> request = parseRequest();
+            if (!request.get("Endpoint").equals("/")){
+                httpStatus = "404 Not Found";
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        output.println( String.format("%s %s \r\n\r\n",HTTP_VERSION,httpStatus ) );
     }
+
+    private Map<String, String> parseRequest() throws IOException {
+        Map<String, String> request = new HashMap<>();
+
+        String[] header = input.readLine().split(" ");
+
+        request.put("Method",header[0]);
+        request.put("Endpoint",header[1]);
+        request.put("HTTP_VERSION",header[2]);
+
+        return request;
+    }
+
 }
