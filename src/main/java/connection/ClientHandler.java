069bbfaeb67530a393bef3b14aeaf5be4e78bc5c
Widua <mikolaj.widla@gmail.com>
2025-07-16 23:32:26 +0200

    Added file submitting

--- src/main/java/connection/ClientHandler.java
+++ src/main/java/connection/ClientHandler.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
@@ -17,32 +18,44 @@ public class ClientHandler implements Runnable {
     private PrintWriter output;
     private BufferedReader input;
     private ServerSettings settings = ServerSettings.getInstance();
+    Map<String,String> request;
     ResponseBuilder response = new ResponseBuilder();
+
     public ClientHandler(PrintWriter output, BufferedReader input) {
         this.output = output;
         this.input = input;
+        try {
+            this.request = parseRequest();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void run() {
         System.out.println("accepted new connection");
         try {
-            Map<String, String> request = parseRequest();
             String endpoint = request.get("Endpoint");
             switch (endpoint) {
                 case String s when Pattern
                         .matches("/echo/[A-z0-9]*", s) -> {
-                    response.setBody(s.replaceAll("/echo/",""),"text/plain");
+                    response.setBody(s.replaceAll("/echo/", ""), "text/plain");
                 }
                 case "/" -> {
 
                 }
-                case String s when Pattern.matches("/files/\\S*",s) -> {
-                    fileStreamer(s.replaceAll("/files/",""));
+                case String s when Pattern.matches("/files/\\S*", s) -> {
+                    String method = request.get("Method");
+
+                    switch (method){
+                        case "GET" -> fileReader(s.replaceAll("/files/", ""));
+                        case "POST" -> fileWriter(s.replaceAll("/files/",""));
+                    }
+
                     return;
                 }
-                case "/user-agent" ->{
-                    response.setBody(request.get("User-Agent"),"text/plain");
+                case "/user-agent" -> {
+                    response.setBody(request.get("User-Agent"), "text/plain");
                 }
                 default -> {
                     response.setHttpStatus("404 Not Found");
@@ -62,41 +75,51 @@ public class ClientHandler implements Runnable {
 
         StringBuilder req = new StringBuilder();
         int s = 0;
-        while (input.ready()){
+        while (input.ready()) {
             s = input.read();
-            req.append((char)s);
+            req.append((char) s);
         }
         String[] splitted = req.toString().split("\r\n");
 
-        if (splitted.length == 1){
-            request.put("Endpoint","/");
+        if (splitted.length == 1) {
+            request.put("Endpoint", "/");
             return request;
         }
         String[] head = splitted[0].split(" ");
 
-        request.put("Method",head[0]);
-        request.put("Endpoint",head[1]);
-        request.put("HTTP_VERSION",head[2]);
+        request.put("Method", head[0]);
+        request.put("Endpoint", head[1]);
+        request.put("HTTP_VERSION", head[2]);
         int eohIndex = 1;
-        for (String header: Arrays.copyOfRange(splitted, 1, splitted.length)) {
-            eohIndex ++;
-            if (header.isEmpty()){
+        for (String header : Arrays.copyOfRange(splitted, 1, splitted.length)) {
+            eohIndex++;
+            if (header.isEmpty()) {
                 break;
             }
             String[] headerCon = header.split(": ");
-            request.put(headerCon[0],headerCon[1]);
+            request.put(headerCon[0], headerCon[1]);
         }
 
-        if (eohIndex < splitted.length){
-            request.put("Body",splitted[eohIndex]);
+        if (eohIndex < splitted.length) {
+            request.put("Body", splitted[eohIndex]);
         }
         return request;
     }
 
-    private void fileStreamer(String fileName) throws IOException {
-        File file = new File(Path.of(settings.getSetting("directory"),fileName).toUri());
+    private void fileWriter(String fileName) throws IOException {
+        File file = new File(Path.of(settings.getSetting("directory"), fileName).toUri());
+        String body = request.get("Body");
+        int contentLength = Integer.parseInt(request.get("Content-Length"));
+        Files.writeString(file.toPath(), body.substring(0,contentLength));
 
-        if (file.exists()){
+        response.setHttpStatus("201 Created");
+        output.println(response);
+    }
+
+    private void fileReader(String fileName) throws IOException {
+        File file = new File(Path.of(settings.getSetting("directory"), fileName).toUri());
+
+        if (file.exists()) {
             byte[] fileContent = Files.readAllBytes(file.toPath());
             StringBuilder builder = response.getResponseHead();
             builder.append("Content-Type: application/octet-stream").append("\r\n");
